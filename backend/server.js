b81cc44c3ee8e9f8195f5308fd87b59053704eba
Pulkit <pulkit161001@gmail.com>
2025-01-10 17:46:23 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const { Server } = require("socket.io");
 require("dotenv").config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
